78709d5c0c9661ab101b716832f839e4c6cd525c
jyi2ya <jyi2ya@outlook.com>
2024-06-10 16:22:46 +0800

    ciallo~

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ struct ScopeTimer {
     start: time::Instant,
 }
 
+#[allow(dead_code)]
 impl ScopeTimer {
     pub fn with_label(label: impl ToString) -> Self {
         Self {
@@ -43,7 +44,7 @@ fn main() {
     let _timer = ScopeTimer::with_label("totals");
 
     let edges_group_by_src = {
-        let _timer = ScopeTimer::with_label("read and parse");
+        let _timer = ScopeTimer::with_label("read and build");
 
         let args: Vec<_> = std::env::args().collect();
         let filename = &args[1];
@@ -52,82 +53,58 @@ fn main() {
         let content = &mmap[..];
         let content = unsafe { std::str::from_utf8_unchecked(content) };
 
-        let mut edges: Vec<_> = content
-            .par_lines()
-            .filter_map(|line| {
-                let line = line.as_bytes();
-                if line[0] == b'#' {
-                    return None;
-                }
-                let (src, dst) = line.split_once(|x| x.is_ascii_whitespace()).unwrap();
-                let src: NodeId = atoi_simd::parse(src).unwrap();
-                let dst: NodeId = atoi_simd::parse(dst).unwrap();
-                if src == dst {
-                    return None;
-                }
-                Some((src.min(dst), src.max(dst)))
-            })
-            .collect();
-        edges.as_mut_slice().par_sort_unstable();
-        edges.dedup();
+        let edges: Vec<_> = {
+            let _timer = ScopeTimer::with_label("scan and parse");
+            content
+                .par_lines()
+                .filter_map(|line| {
+                    let line = line.as_bytes();
+                    if line[0] == b'#' {
+                        return None;
+                    }
+                    let (src, dst) = line.split_once(|x| x.is_ascii_whitespace()).unwrap();
+                    let src: NodeId = atoi_simd::parse(src).unwrap();
+                    let dst: NodeId = atoi_simd::parse(dst).unwrap();
+                    if src == dst {
+                        return None;
+                    }
+                    Some((src.max(dst), src.min(dst)))
+                })
+                .collect()
+        };
+
+        let edges = {
+            let _timer = ScopeTimer::with_label("sort and dedup");
+            let mut edges = edges;
+            edges.as_mut_slice().par_sort_unstable();
+            edges.dedup();
+            edges
+        };
+
         edges
     };
 
-    // let edges_group_by_dst_handle = {
-    //     let mut edges = edges_group_by_src.clone();
-    //     thread::spawn(move || {
-    //         let _timer = ScopeTimer::with_label("sort by dst");
-    //         edges
-    //             .as_mut_slice()
-    //             .par_sort_unstable_by_key(|(src, dst)| (*dst, *src));
-    //         edges
-    //     })
-    // };
+    let max_node_id = edges_group_by_src.last().unwrap().0 as usize;
 
-    let timer_pre_computing = ScopeTimer::with_label("counting neighbors");
-
-    let max_node_id = edges_group_by_src
-        .iter()
-        .map(|(_, bigger)| bigger)
-        .max()
-        .cloned()
-        .map(|x| x as usize)
-        .unwrap();
-
-    let uppers = {
-        let mut uppers: Vec<Vec<NodeId>> = Vec::new();
-        uppers.resize(max_node_id + 1, Vec::default());
+    let lowers = {
+        let _timer = ScopeTimer::with_label("counting neighbors");
+        let mut lowers: Vec<Vec<NodeId>> = Vec::new();
+        lowers.resize(max_node_id + 1, Vec::default());
 
         for (src, dst) in edges_group_by_src {
-            uppers[src as usize].push(dst);
+            lowers[src as usize].push(dst);
         }
 
-        uppers
+        lowers
     };
-
-    timer_pre_computing.stop();
-
-    // let lowers_handle = thread::spawn(move || {
-    //     let mut lowers: Vec<Vec<NodeId>> = Vec::new();
-    //     lowers.resize(max_node_id + 1, Vec::default());
-
-    //     for (src, dst) in edges_group_by_dst_handle.join().unwrap() {
-    //         lowers[dst as usize].push(src);
-    //     }
-
-    //     lowers
-    // });
-
-    // let uppers = uppers_handle.join().unwrap();
-    // let _lowers = lowers_handle.join().unwrap();
 
     const TRUNK_SIZE_RATIO: usize = 32;
 
     let result: usize = {
         let _timer = ScopeTimer::with_label("computation");
-        uppers
+        lowers
             .as_slice()
-            .par_chunks(uppers.len() / (TRUNK_SIZE_RATIO * rayon::current_num_threads()))
+            .par_chunks(lowers.len() / (TRUNK_SIZE_RATIO * rayon::current_num_threads()))
             .map(|data| {
                 let mut bitmap = bitvec![];
                 bitmap.resize(max_node_id + 1, false);
@@ -141,7 +118,7 @@ fn main() {
 
                     result += first
                         .iter()
-                        .flat_map(|second| uppers[*second as usize].iter())
+                        .flat_map(|second| lowers[*second as usize].iter())
                         .map(|third| bitmap[*third as usize] as usize)
                         .sum::<usize>();
 
